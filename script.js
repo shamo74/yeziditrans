e993193f4c9b1b7e73430808d6f9aa4909cd9577
shamo74 <send.shamo@gmail.com>
2024-07-18 16:57:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const SPECIAL_RESPONSES = {};
 
             // جلب محتوى ملف JSON
-            fetch('translationss.json')
+            fetch('translations.json')
                 .then(response => response.json())
                 .then(data => {
                     // تحديث inukKey و SPECIAL_RESPONSES بالمحتوى المسترجع
